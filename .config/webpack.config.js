73b7dcdfece5a90fad10f3670e4428e8dd3fbabd
Simon Stucki <simon.stucki@goldinteractive.ch>
2018-02-16 09:58:49 +0100

    fix webpack build

--- .config/webpack.config.js
+++ .config/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
     (context, request, callback) => {
       if (/^gi-.+$/.test(request)){
         // TODO generate define name based on required package...
-        callback(null, request)
+        return callback(null, request)
       }
       callback();
     }
