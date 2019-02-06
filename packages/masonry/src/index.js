940192b523f20a103eaf6954a545d8d5f3fc6406
Simon Stucki <simon_stucki@hotmail.com>
2019-02-06 16:53:15 +0100

    Improve Masonry Feature options
    Enable consumer to overwrite all masonry options.

--- packages/masonry/src/index.js
+++ packages/masonry/src/index.js
@@ -3,28 +3,29 @@ import { features } from '@goldinteractive/js-base'
 import MasonryLayout from 'masonry-layout'
 
 class Masonry extends features.Feature {
-
   init() {
-    this.node.classList.remove(this.options.hiddenClassName);
-    this.masonry = new MasonryLayout(this.node, {
-      itemSelector: this.options.itemSelector,
-      columnWidth: this.options.columnWidth,
-      percentPosition: this.options.percentPosition
-    })
+    this.node.classList.remove(this.options.hiddenClassName)
+    const masonryOptions = Object.assign(
+      {},
+      Masonry.defaultOptions.masonryOptions,
+      this.options.masonryOptions
+    )
+    this.masonry = new MasonryLayout(this.node, masonryOptions)
   }
 
   destroy() {
     super.destroy()
     this.masonry.destroy()
   }
-
 }
 
 Masonry.defaultOptions = {
-  itemSelector: '.grid-item',
-  columnWidth: '.grid-sizer',
-  percentPosition: true,
-  hiddenClassName: '-hidden'
+  hiddenClassName: '-hidden',
+  masonryOptions: {
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    percentPosition: true
+  }
 }
 
 export default Masonry
