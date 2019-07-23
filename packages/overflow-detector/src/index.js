8eb3d0673aada597e32d3f327a37871649972b5d
Simon Stucki <simon_stucki@hotmail.com>
2019-07-23 09:32:49 +0200

    OverflowDetector: Add initial implementation
    This feature enables detecting overflow on nodes and applying classes
    on a given node.

---
+++ packages/overflow-detector/src/index.js
@@ -0,0 +1,66 @@
+import { features, utils, eventHub } from '@goldinteractive/js-base'
+
+export class OverflowStrategy {
+  /**
+   * Strategy constructor
+   * @param {Object} configuration
+   */
+  constructor({ classNamespace = 'global', $node = document.body } = {}) {
+    this.classNamespace = classNamespace
+    this.$node = $node
+    this.retryOverflow = this._retryOverflow()
+    eventHub.on('features:initialized', utils.fn.once(this.retryOverflow))
+    window.addEventListener('resize', utils.fn.debounce(this.retryOverflow))
+    this._instances = []
+  }
+
+  _retryOverflow() {
+    return () => {
+      this.$node.classList.add(`${this.classNamespace}--hidden`)
+      this.$node.classList.remove(`${this.classNamespace}--overflow`)
+      const isAnyOverflowing = this._instances.some(instance =>
+        instance.isOverflowing()
+      )
+      if (isAnyOverflowing) {
+        this.$node.classList.add(`${this.classNamespace}--overflow`)
+
+        // accessing the dom node forces relayout to take effect, preventing animation flickering
+        this.$node.offsetHeight
+      }
+
+      this.$node.classList.remove(`${this.classNamespace}--hidden`)
+    }
+  }
+
+  register(instance) {
+    this._instances.push(instance)
+  }
+}
+
+/**
+ * Overflow Detector Feature
+ *
+ * currently `destroy` is not handled for this feature.
+ */
+class OverflowDetector extends features.Feature {
+  init() {
+    if (this.options.strategy === null) {
+      throw new Error('Strategy must not be null, got', this.options.strategy)
+    }
+    this.options.strategy.register(this)
+  }
+  isOverflowing() {
+    return this.node.scrollWidth > this.node.clientWidth
+  }
+}
+
+/**
+ * Feature options
+ * @type {Object}
+ * @property {OverflowStrategy} strategy overflow handling strategy.
+ */
+OverflowDetector.defaultOptions = {
+  strategy: null
+}
+
+export default OverflowDetector
