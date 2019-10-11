fcdfd7ee80240eacb3805d36ed29bf6955370be3
Michel Gabriel <michel.gabriel@goldinteractive.ch>
2019-10-11 09:32:53 +0200

    Remove comments and console logs

--- packages/reveal-trigger/src/strategies/lazy-video.js
+++ packages/reveal-trigger/src/strategies/lazy-video.js
@@ -8,29 +8,22 @@ class LazyVideoStrategy {
     return ({ node, entry }) => {
       if (entry.isIntersecting) {
         if (node.classList.contains('-lazy')) {
-          node.addEventListener('timeupdate', () => {
-            console.log(node.currentTime)
-          })
           node.children.forEach(child => {
             if (child.src === '') {
               child.src = child.dataset.src
             }
           })
-          console.log('loaded src')
           node.load()
           node.classList.remove('-lazy')
         } else {
-          console.log('play')
           node.play()
         }
       } else {
-        console.log('pause')
         node.pause()
       }
     }
   }
 
-  // Gets called from reveal-trigger
   register(node, manager) {
     manager.register(node, this.onNotify)
   }
