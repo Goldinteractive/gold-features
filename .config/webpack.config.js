632695beb05cb99af05a7520ffb72e48e717fe95
Marc <marc.berli@windowslive.com>
2020-07-21 13:54:21 +0200

    Enable nullish coalescing and optional chaining

--- .config/webpack.config.js
+++ .config/webpack.config.js
@@ -79,7 +79,9 @@ module.exports = () => {
                     '@babel/plugin-proposal-decorators',
                     { decoratorsBeforeExport: true }
                   ],
-                  '@babel/plugin-proposal-class-properties'
+                  '@babel/plugin-proposal-class-properties',
+                  '@babel/plugin-proposal-optional-chaining',
+                  '@babel/plugin-proposal-nullish-coalescing-operator'
                 ]
               }
             }
