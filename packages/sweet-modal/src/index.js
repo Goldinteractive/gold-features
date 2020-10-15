753c4da0a60a49c003164504c2cdeb8f3f92562a
MitchGabriel <40825562+MitchGabriel@users.noreply.github.com>
2020-10-15 09:27:51 +0200

    Feature sweet modal (#28)
    Add a new modal feature based on sweetalert2
    Allow content from html templates, ajax endpoints or video source

---
+++ packages/sweet-modal/src/index.js
@@ -0,0 +1,128 @@
+import { features } from '@goldinteractive/js-base'
+import { json } from '@goldinteractive/js-base/src/utils/fetch'
+import Swal from 'sweetalert2'
+
+class SweetModal extends features.Feature {
+  init() {
+    this.modalIdentifier =
+      this.node.dataset.modalIdentifier || this.options.modalIdentifier
+    if (!this.modalIdentifier) {
+      throw new Error(
+        `SweetModal" ${
+          this.name
+        }" feature needs to be initialized with a modal-identifier`
+      )
+    }
+    this.html = ''
+
+    this.registerEvents()
+    this.handleOpenOnLoad()
+  }
+
+  registerEvents = () => {
+    this.addEventListener(this.node, 'click', this.clickHandler)
+    this.onHub(`${this.modalIdentifier}:open`, this.openHandler)
+    this.onHub(`${this.modalIdentifier}:close`, this.closeHandler)
+  }
+
+  clickHandler = e => {
+    e.preventDefault()
+    this.triggerHub(`${this.node.dataset.modalIdentifier}:open`)
+  }
+
+  handleOpenOnLoad = () => {
+    if (this.options.openOnLoad) {
+      this.triggerHub(`${this.modalIdentifier}:open`)
+    }
+  }
+
+  getData = () => {
+    if (this.node.dataset.videoUrl) {
+      this._getVideo()
+    } else if (this.options.htmlContentSelector) {
+      this._getHtmlByTemplate()
+    } else if (this.options.endpoint) {
+      this._getHtmlByEndpoint()
+    }
+  }
+
+  _getVideo = () => {
+    const url = this.node.dataset.videoUrl
+    let $iframe  = document.createElement("iframe");
+    $iframe.src = url
+    $iframe.classList.add('iframe')
+    this.html = $iframe.outerHTML
+  }
+
+  _getHtmlByTemplate = () => {
+    const globalContentSelector = `${
+      this.options.htmlContentSelector
+    }[data-modal-identifier="${this.modalIdentifier}"]`
+
+    this.$content =
+      this.$(`${this.options.htmlContentSelector}`) ??
+      document.querySelector(globalContentSelector)
+
+    if (!this.$content) {
+      throw new Error(
+        `SweetModal" ${this.name}": html content element not found`
+      )
+    }
+    this.html = this.$content.innerHTML
+  }
+
+  _getHtmlByEndpoint = async () => {
+    const result = await json(this.options.endpoint)
+    this.html = result.html
+  }
+
+  openHandler = () => {
+    this.getData()
+    if (this.options.delay <= 0) {
+      this._open()
+    } else {
+      setTimeout(() => {
+        this._open()
+      }, this.options.delay)
+    }
+  }
+
+  _open = () => {
+    Swal.fire({
+      html: this.html,
+      showConfirmButton: false,
+      showCloseButton: true,
+      willOpen: () => {
+        this.triggerHub(`${this.modalIdentifier}:will-open`)
+      },
+      didOpen: () => {
+        this.triggerHub(`${this.modalIdentifier}:did-open`)
+      },
+      willClose: () => {
+        this.triggerHub(`${this.modalIdentifier}:will-close`)
+      },
+      didClose: () => {
+        this.triggerHub(`${this.modalIdentifier}:did-close`)
+      },
+      didRender: () => {
+        this.triggerHub(`${this.modalIdentifier}:did-render`)
+      },
+      ...this.options.swalConfig
+    })
+  }
+
+  closeHandler = () => {
+    Swal.close()
+  }
+}
+
+SweetModal.defaultOptions = {
+  htmlContentSelector: '[data-sweet-modal-content]',
+  endpoint: null,
+  modalIdentifier: null,
+  openOnLoad: false,
+  delay: 0,
+  swalConfig: {}
+}
+
+export default SweetModal
---
+++ packages/sweet-modal/stories/index.stories.js
@@ -0,0 +1,178 @@
+import { storiesOf } from '@storybook/html'
+import { withKnobs, object } from '@storybook/addon-knobs'
+
+import {
+  initializeDemo,
+  styleSource,
+  resetFeature
+} from '../../../helpers/story'
+
+import { features, eventHub } from '@goldinteractive/js-base'
+
+import SweetModal from '../src/index'
+import '../src/style.scss'
+
+import docs from './docs.md'
+import testJpg from './files/test.jpeg'
+
+const styles = `
+  <style>
+    .ft-sweet-modal {
+      display: none;
+    }
+
+    h2 {
+      font-size: 25px;
+      color: blue;
+    }
+
+    .swal2-content {
+      text-align: left;
+    }
+
+    .iframe {
+      width: 100%;
+      height: 20vh;
+    }
+  </style>
+`
+
+const markupIntro = `
+${styles}
+<div class="content" data-sweet-modal-content>
+  <h2>Sweet Modal</h2>
+  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+  <img src="${testJpg}" width="200" />
+</div>
+<button data-feature="sweet-modal" data-modal-identifier="sample-id" data-cy="trigger">Trigger</button>
+<div data-modal-state>
+  <h3>States log:</h3>
+</div>
+`
+
+const markupOpenOnLoad = `
+${styles}
+<div class="" data-feature="sweet-modal" data-modal-identifier="sample-id"></div>
+<div class="content" data-sweet-modal-content>
+  <h2>Open on load by delay</h2>
+  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+  <img src="${testJpg}" width="200" />
+</div>
+<div data-modal-state>
+  <h3>States log:</h3>
+</div>
+`
+
+const markupVideo = `
+${styles}
+<button data-feature="sweet-modal" data-modal-identifier="sample-id1" data-video-url="https://www.youtube.com/embed/ScMzIvxBSi4?rel=0">Trigger 1</button>
+<button data-feature="sweet-modal" data-modal-identifier="sample-id2" data-video-url="https://www.youtube.com/embed/y2Ky3Wo37AY?rel=0">Trigger 2</button>
+<button data-feature="sweet-modal" data-modal-identifier="sample-id3" data-video-url="https://www.youtube.com/embed/zlRl8sJU_4I?rel=0">Trigger 3</button>
+`
+
+storiesOf('SweetModal', module)
+  .addDecorator(withKnobs)
+  .add(
+    'Intro',
+    () => {
+      return initializeDemo(markupIntro, () => {
+        resetFeature(features, 'sweet-modal')
+        features.add('sweet-modal', SweetModal)
+        features.init(document.body)
+        initEvents()
+      })
+    },
+    {
+      notes: {
+        markdown: docs
+      }
+    }
+  )
+  .add(
+    'OpenOnLoadWithDelay',
+    () => {
+      return initializeDemo(markupOpenOnLoad, () => {
+        resetFeature(features, 'sweet-modal')
+        features.add('sweet-modal', SweetModal, {
+          openOnLoad: true,
+          delay: 2000
+        })
+        features.init(document.body)
+
+        setTimeout(() => {
+          eventHub.trigger('sample-id:close')
+        }, 6000);
+        initEvents()
+      })
+    },
+    {
+      notes: {
+        markdown: docs
+      }
+    }
+  )
+  .add(
+    'Video',
+    () => {
+      return initializeDemo(markupVideo, () => {
+        resetFeature(features, 'sweet-modal')
+        features.add('sweet-modal', SweetModal, {
+          useVideo: false,
+          swalConfig: {
+            width: 'auto',
+          }
+        })
+        features.init(document.body)
+      })
+    },
+    {
+      notes: {
+        markdown: docs
+      }
+    }
+  )
+  .add('Source JS', () => {
+    return styleSource({ feature: 'sweet-modal' })
+  })
+  .add('Source CSS', () => {
+    return styleSource({ feature: 'sweet-modal', language: 'sass' })
+  })
+
+  const initEvents = () => {
+    const $modalState = document.querySelector('[data-modal-state]')
+    eventHub.on('sample-id:open',() => {
+      let p = document.createElement("p");
+      p.textContent = 'open'
+      $modalState.appendChild(p)
+    })
+    eventHub.on('sample-id:close',() => {
+      let p = document.createElement("p");
+      p.textContent = 'close'
+      $modalState.appendChild(p)
+    })
+    eventHub.on('sample-id:will-open',() => {
+      let p = document.createElement("p");
+      p.textContent = 'will-open'
+      $modalState.appendChild(p)
+    })
+    eventHub.on('sample-id:did-open',() => {
+      let p = document.createElement("p");
+      p.textContent = 'did-open'
+      $modalState.appendChild(p)
+    })
+    eventHub.on('sample-id:will-close',() => {
+      let p = document.createElement("p");
+      p.textContent = 'will-close'
+      $modalState.appendChild(p)
+    })
+    eventHub.on('sample-id:did-close',() => {
+      let p = document.createElement("p");
+      p.textContent = 'did-close'
+      $modalState.appendChild(p)
+    })
+    eventHub.on('sample-id:did-render',() => {
+      let p = document.createElement("p");
+      p.textContent = 'did-render'
+      $modalState.appendChild(p)
+    })
+  }
