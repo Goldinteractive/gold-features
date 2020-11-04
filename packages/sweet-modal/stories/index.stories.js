e3a075fdaa7f90439f123bb4bf5367f5f746c27d
Michel Gabriel <michel.gabriel@goldinteractive.ch>
2020-11-04 16:17:13 +0100

    Make it optional to add ad contentSelector to HtmlTemplate strategy
    If no selector is given the content is considered to be inside the feature node
    
    Update story to show new behaviour

--- packages/sweet-modal/stories/index.stories.js
+++ packages/sweet-modal/stories/index.stories.js
@@ -57,9 +57,8 @@ ${styles}
 
 const markupOpenOnLoad = `
 ${styles}
-<div class="" data-feature="sweet-modal" data-modal-identifier="sample-id"></div>
 <div class="hide">
-  <div class="content" data-sweet-modal-content data-modal-identifier="sample-id">
+  <div class="content" data-feature="sweet-modal" data-modal-identifier="sample-id">
     <h2>Open on load by delay</h2>
     <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
     <img src="${testJpg}" width="200" />
