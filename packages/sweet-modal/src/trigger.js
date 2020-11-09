01380f9ec2679316b618d774be384660d6263c5b
Michel Gabriel <michel.gabriel@goldinteractive.ch>
2020-11-09 11:42:39 +0100

    Add error handling if modal identifier is missing

--- packages/sweet-modal/src/trigger.js
+++ packages/sweet-modal/src/trigger.js
@@ -1,14 +1,21 @@
 import { features } from '@goldinteractive/js-base'
 
-
 class SweetModalTrigger extends features.Feature {
   init() {
+    this.modalIdentifier = this.node.dataset.modalIdentifier
+    if (!this.modalIdentifier) {
+      throw new Error(
+        `SweetModalTrigger" ${
+          this.name
+        }" feature needs to be initialized with a modal-identifier`
+      )
+    }
     this.addEventListener(this.node, 'click', this._clickHandler)
   }
 
-  _clickHandler = (event) => {
-    event.preventDefault() // do not change scroll position or follow link
-    this.triggerHub(`${this.node.dataset.modalIdentifier}:open`, {})
+  _clickHandler = e => {
+    e.preventDefault() // do not change scroll position or follow link
+    this.triggerHub(`${this.modalIdentifier}:open`, {})
   }
 }
 
