9f3f4c063d7d9f19fcea6ee3ac07bb4db541429d
Michel Gabriel <michel.gabriel@goldinteractive.ch>
2021-04-19 12:51:46 +0200

    Use data from json and more stuff

--- packages/content-pagination/src/index.js
+++ packages/content-pagination/src/index.js
@@ -1,61 +1,45 @@
 import { features } from '@goldinteractive/js-base'
+import { json } from '@goldinteractive/js-base/src/utils/fetch'
 
 class ContentPagination extends features.Feature {
   init() {
     this.$content = this.$('[data-content]')
     this.$previous = this.$('[data-previous]')
     this.$next = this.$('[data-next]')
-    this.maxItems = 20 // temp
-    this.currentPage = 0
+    this.currentPage = 1
     this.maxPages = 4 // get from endpoint
     this.initial = true // temp
+    this.state = ''
 
     this.onHub(`${this.options.namespace}:state-update`, state =>
       this.handleStateUpdate(state)
     )
 
-    this.addEventListener(this.$previous, 'click', this.handlePrevious())
-    this.addEventListener(this.$next, 'click', this.handleNext())
+    this.addEventListener(this.$previous, 'click', this.handlePrevious)
+    this.addEventListener(this.$next, 'click', this.handleNext)
   }
 
-  fetchContent(parameters = '') {
-    fetch(`https://jsonplaceholder.typicode.com/posts/`)
-      .then(response => response.json())
-      .then(json => this.injectContent(json))
-  }
-
-  injectContent = data => {
-    console.log(data)
-    let counter = 0
-    this.$content.innerHTML = ''
-    data.forEach(item => {
-      counter++
-      if (counter <= this.maxItems) {
-        const $div = document.createElement('div')
-        $div.classList.add('item')
-        const $h3 = document.createElement('h3')
-        const h3text = document.createTextNode(item.title || item.email)
-        $h3.appendChild(h3text)
-        const $p = document.createElement('p')
-        const ptext = document.createTextNode(item.body + ' ' + counter)
-        $p.appendChild(ptext)
-        const $pcounter = document.createElement('p')
-        const pcounter = document.createTextNode(counter)
-        $pcounter.appendChild(pcounter)
-
-        $div.appendChild($h3)
-        $div.appendChild($p)
-        $div.appendChild($pcounter)
-        this.$content.appendChild($div)
-      }
+  getData(parameters = '') {
+    console.log('parameters:', parameters)
+    const url = './data.json'
+    const data = require(`${url}`) // TODO: add paramets
+    json(data).then(result => {
+      this.injectContent(result.data.page[this.currentPage - 1].html) // temp
     })
+  }
+
+  injectContent = html => {
+    console.log(html)
+    this.$content.innerHTML = html
   }
 
   buildQueryParam = () => {
     const stateParameters = this.transformState()
     const pageParameters =
-      this.currentPage > 0 ? `&page=${this.currentPage}` : ''
+      this.currentPage > 1 ? `&page=${this.currentPage}` : ''
     const parameters = `${stateParameters}${pageParameters}`
+    this.getData(parameters)
+    // TODO: inject link to prev/next buttons
   }
 
   handleStateUpdate = state => {
@@ -67,14 +51,16 @@ class ContentPagination extends features.Feature {
     this.buildQueryParam()
   }
 
-  handlePrevious = () => {
-    if (this.currentPage > 0) {
+  handlePrevious = e => {
+    e.preventDefault()
+    if (this.currentPage > 1) {
       this.currentPage--
       this.buildQueryParam()
     }
   }
 
-  handleNext = () => {
+  handleNext = e => {
+    e.preventDefault()
     if (this.currentPage < this.maxPages) {
       this.currentPage++
       this.buildQueryParam()
