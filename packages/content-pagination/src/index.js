f256c43c6df76dab6117d5632f78d3fe931b702b
Michel Gabriel <michel.gabriel@goldinteractive.ch>
2021-04-22 10:59:34 +0200

    add page links manual handling (wip)

--- packages/content-pagination/src/index.js
+++ packages/content-pagination/src/index.js
@@ -1,5 +1,5 @@
 import { features } from '@goldinteractive/js-base'
-import { json } from '@goldinteractive/js-base/src/utils/fetch'
+import { utils } from '@goldinteractive/js-base'
 
 class ContentPagination extends features.Feature {
   init() {
@@ -11,35 +11,54 @@ class ContentPagination extends features.Feature {
     this.initial = true // temp
     this.state = ''
 
-    this.onHub(`${this.options.namespace}:state-update`, state =>
-      this.handleStateUpdate(state)
-    )
+    this.onHub(`${this.options.namespace}:state-update`, state => this.handleStateUpdate(state))
 
     this.addEventListener(this.$previous, 'click', this.handlePrevious)
     this.addEventListener(this.$next, 'click', this.handleNext)
+
+    this.handlePreviousNextButtonsDisplay()
   }
 
   getData(parameters = '') {
     console.log('parameters:', parameters)
     const url = './data.json'
-    const data = require(`${url}`) // TODO: add paramets
-    json(data).then(result => {
+    const data = require(`${url}`) // TODO: add parameters
+    utils.fetch.json(data).then(result => {
       this.injectContent(result.data.page[this.currentPage - 1].html) // temp
     })
   }
 
   injectContent = html => {
-    console.log(html)
     this.$content.innerHTML = html
   }
 
+  buildPreviousNextLinks = () => {
+    console.log('currentpage:', this.currentPage)
+    const queryString = this.persistState()
+    
+    const previousPageString = `&page=${this.currentPage - 1}`
+    const nextPage = `&page=${this.currentPage + 1}`
+    const previousPageUrl = location.origin + location.pathname + '?' + queryString + previousPageString + location.hash
+    const nextPageUrl = location.origin + location.pathname + '?' + queryString + nextPage + location.hash
+
+    this.$previous.href = previousPageUrl
+    this.$next.href = nextPageUrl
+  }
+
+  persistState = () => {
+    let queryString = ''
+    if (Object.keys(this.state).length > 0) {
+      const query = Object.assign({}, utils.url.parseQuery(location.search, { ignoreQueryPrefix: true }), this.state)
+      queryString = utils.url.stringifyQuery(query)
+    }
+    return queryString
+  }
+
   buildQueryParam = () => {
-    const stateParameters = this.transformState()
-    const pageParameters =
-      this.currentPage > 1 ? `&page=${this.currentPage}` : ''
-    const parameters = `${stateParameters}${pageParameters}`
+    const queryString = this.persistState()
+    const pageParameters = this.currentPage > 1 ? `&page=${this.currentPage}` : ''
+    const parameters = `${queryString}${pageParameters}`
     this.getData(parameters)
-    // TODO: inject link to prev/next buttons
   }
 
   handleStateUpdate = state => {
@@ -47,8 +66,10 @@ class ContentPagination extends features.Feature {
       this.initial = false
       return
     }
-    this.state = state
+    this.state = this.transformState(state)
     this.buildQueryParam()
+    this.buildPreviousNextLinks()
+    this.handlePreviousNextButtonsDisplay()
   }
 
   handlePrevious = e => {
@@ -56,6 +77,8 @@ class ContentPagination extends features.Feature {
     if (this.currentPage > 1) {
       this.currentPage--
       this.buildQueryParam()
+      this.buildPreviousNextLinks()
+      this.handlePreviousNextButtonsDisplay()
     }
   }
 
@@ -64,12 +87,26 @@ class ContentPagination extends features.Feature {
     if (this.currentPage < this.maxPages) {
       this.currentPage++
       this.buildQueryParam()
+      this.buildPreviousNextLinks()
+      this.handlePreviousNextButtonsDisplay()
     }
   }
 
-  transformState = () => {
-    const splitState = this.state.split('%2C')
-    return splitState.reduce((acc, state) => `${acc}&${state}`)
+  transformState = (state) => {
+    return utils.url.parseQuery(state)
+  }
+
+  handlePreviousNextButtonsDisplay = () => {
+    if (this.currentPage == 1) {
+      this.$previous.classList.add('-hide')
+    } else {
+      this.$previous.classList.remove('-hide')
+    }
+    if (this.currentPage == this.maxPages) {
+      this.$next.classList.add('-hide')
+    } else {
+      this.$next.classList.remove('-hide')
+    }
   }
 }
 
