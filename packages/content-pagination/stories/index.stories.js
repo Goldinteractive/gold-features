8c65b4c967d9573feda09d10eb57d9d15ad26247
Michel Gabriel <michel.gabriel@goldinteractive.ch>
2021-04-26 15:53:25 +0200

    Remove test data and improve story content, use staticStrategy

--- packages/content-pagination/stories/index.stories.js
+++ packages/content-pagination/stories/index.stories.js
@@ -5,59 +5,13 @@ import { initializeDemo, styleSource, resetFeature } from '../../../helpers/stor
 
 import { features, eventHub } from '@goldinteractive/js-base'
 
-import ContentPagination, { ContentStrategies } from '../src/index'
+import ContentPagination from '../src/index'
+import StaticStrategy from './files/staticStrategy'
 import DomStateHandler from '../../dom-state-handler/src/index'
 import { UrlParameter } from '../../dom-state-handler/src/persistors/index'
 import '../src/style.scss'
 
 import docs from './docs.md'
-
-const dataPage1 = {
-  html:
-    '<div class="item"><h3>Titel 1</h3><p>test 1</p></div><div class="item"><h3>Titel 2</h3><p>test 2</p></div><div class="item"><h3>Titel 3</h3><p>test 3</p></div>',
-  meta: {
-    skip: 0,
-    take: 3,
-    total: 13
-  }
-}
-const dataPage2 = {
-  html:
-    '<div class="item"><h3>Titel 4</h3><p>test 4</p></div><div class="item"><h3>Titel 5</h3><p>test 5</p></div><div class="item"><h3>Titel 6</h3><p>test 6</p></div>',
-  meta: {
-    skip: 3,
-    take: 3,
-    total: 13
-  }
-}
-const dataPage3 = {
-  html:
-    '<div class="item"><h3>Titel 7</h3><p>test 7</p></div><div class="item"><h3>Titel 8</h3><p>test 8</p></div><div class="item"><h3>Titel 9</h3><p>test 9</p></div>',
-  meta: {
-    skip: 6,
-    take: 3,
-    total: 13
-  }
-}
-const dataPage4 = {
-  html:
-    '<div class="item"><h3>Titel 10</h3><p>test 10</p></div><div class="item"><h3>Titel 11</h3><p>test 11</p></div><div class="item"><h3>Titel 12</h3><p>test 12</p></div>',
-  meta: {
-    skip: 9,
-    take: 3,
-    total: 13
-  }
-}
-const dataPage5 = {
-  html: '<div class="item"><h3>Titel 13</h3><p>test 13</p></div>',
-  meta: {
-    skip: 12,
-    take: 3,
-    total: 13
-  }
-}
-
-export const allData = [dataPage1, dataPage2, dataPage3, dataPage4, dataPage5]
 
 const styles = `
   <style>
@@ -92,33 +46,34 @@ ${styles}
   <h2>Content Pagination</h2>
   <div class="filters" data-feature="dom-state-handler" data-state-handler-type="checkbox-group">
     <div>
-      <input type="checkbox" id="option-1" name="option" value="option-1"/>
-      <label for="option-1">Option 1</label>
+      <input type="checkbox" id="violet" name="color" value="violet"/>
+      <label for="violet">Violet</label>
     </div>
     <div>
-      <input type="checkbox" id="option-2" name="option" value="option-2">
-      <label for="option-2">Option 2</label>
+      <input type="checkbox" id="orange name="color" value="orange>
+      <label for="orange>Orange</label>
     </div>
     <div>
-      <input type="checkbox" id="option-3" name="option" value="option-3">
-      <label for="option-3">Option 2</label>
+      <input type="checkbox" id="yellow" name="color" value="yellow">
+      <label for="yellow">Yellow</label>
     </div>
   </div>
   <div class="filters" data-feature="dom-state-handler" data-state-handler-type="radio-group">
-    <input id="orange" type="radio" name="fruit" value="orange" />
-    <label for="orange">orange</label>
-    <input id="violet" type="radio" name="fruit" value="violet" />
-    <label for="violet">violet</label>
-    <input id="yellow" type="radio" name="fruit" value="yellow" />
-    <label for="yellow">yellow</label>
+    <input id="apple" type="radio" name="fruit" value="apple" />
+    <label for="apple">Apple</label>
+    <input id="mango" type="radio" name="fruit" value="mango" />
+    <label for="mango">Mango</label>
+    <input id="grapes" type="radio" name="fruit" value="grapes" />
+    <label for="grapes">Grapes</label>
   </div>
   <div data-feature="content-pagination">
     <div data-content>
       Init
     </div>
     <div class="flex" data-feature="dom-state-handler" data-state-handler-type="button-group">
-     <button data-previous type="submit" name="skip" value="minus">Previous</button>
-     <button data-next type="submit" name="skip" value="plus">Next</button>
+     <button data-previous type="submit" name="skip" value="0">Previous</button>
+     <button data-dom-state-handler-master type="submit" name="skip" value="0">Current</button>
+     <button data-next type="submit" name="skip" value="0">Next</button>
     </div>
   </div>
 </div>
@@ -136,7 +91,7 @@ storiesOf('ContentPagination', module)
           'content-pagination',
           ContentPagination,
           object('options', {
-            strategy: null
+            strategy: new StaticStrategy()
           })
         )
         features.add('dom-state-handler', DomStateHandler, {
