06983724821e0a5c8f16747529132a40acbb6033
Michel Gabriel <michel.gabriel@goldinteractive.ch>
2021-04-26 15:54:33 +0200

    Use passed functions to update buttonhandlers, use staticStrategy, cleanup

--- packages/content-pagination/src/index.js
+++ packages/content-pagination/src/index.js
@@ -5,85 +5,52 @@ import { allData } from '../stories/index.stories'
 class ContentPagination extends features.Feature {
   init() {
     this.$content = this.$('[data-content]')
-    this.$previous = this.$('[data-previous]')
-    this.$next = this.$('[data-next]')
-    this.skip = 0
-    this.maxPages = 4 // get from endpoint
-    this.initial = true // temp
     this.state = ''
     this.queryString = ''
 
     this.onHub(`${this.options.namespace}:state-update`, state => this.handleStateUpdate(state))
-
-    this.handlePreviousNextButtonsDisplay()
-  }
-
-  // Temp
-  manuallyHandleContent = () => {
-    return allData.filter(item => item.meta.skip === this.skip)
-  }
-
-  updateContent = () => {
-    console.log('parameters:', this.queryString)
-    const data = this.manuallyHandleContent()
-    console.log('data received', data)
-    this.injectContent(data[0])
-  }
-
-  injectContent = data => {
-    this.$content.innerHTML = data.html
-  }
-
-  persistState = () => {
-    let queryString = ''
-    if (Object.keys(this.state).length > 0) {
-      const query = Object.assign({}, utils.url.parseQuery(location.search, { ignoreQueryPrefix: true }), this.state)
-      queryString = utils.url.stringifyQuery(query)
-    }
-    return queryString
-  }
-
-  handleSkipState = () => {
-    console.log(this.state)
-    if (this.state.skip === 'plus') {
-      this.skip += 3
-    } else if (this.state.skip === 'minus') {
-      this.skip -= 3
-    } else {
-      this.skip = 0
-    }
   }
 
   handleStateUpdate = state => {
     this.state = this.transformState(state)
     this.queryString = state
-    this.handleSkipState()
     this.updateContent()
-    this.handlePreviousNextButtonsDisplay()
   }
 
   transformState = state => {
     return utils.url.parseQuery(state)
   }
 
-  handlePreviousNextButtonsDisplay = () => {
-    if (this.currentPage == 1) {
-      this.$previous.classList.add('-hide')
-    } else {
-      this.$previous.classList.remove('-hide')
-    }
-    if (this.currentPage == this.maxPages) {
-      this.$next.classList.add('-hide')
-    } else {
-      this.$next.classList.remove('-hide')
-    }
+  updateContent = () => {
+    this.options.strategy.getData(this.injectContent, this.node, this.state)
+  }
+
+  injectContent = data => {
+    console.log('received', data)
+    this.$content.innerHTML = data.html
+    this.updateButtonHandler(data)
+  }
+
+  updateButtonHandler = data => {
+    Object.values(this.options.buttonUpdateHandler).forEach(func => {
+      func(this.node, data)
+    })
   }
 }
 
 ContentPagination.defaultOptions = {
   namespace: 'content-pagination',
-  strategy: null
+  strategy: null,
+  buttonUpdateHandler: {
+    previous: (node, data) => {
+      const $previous = node.querySelector('[data-previous]')
+      $previous.value = data.meta.current - data.meta.take
+    },
+    next: (node, data) => {
+      const $next = node.querySelector('[data-next]')
+      $next.value = data.meta.current + data.meta.take
+    }
+  }
 }
-export default ContentPagination
 
-export { default as ContentStrategies } from './strategies'
+export default ContentPagination
