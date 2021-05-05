23790fe475244ca1f3a639779d37784b1511e19b
Michel Gabriel <michel.gabriel@goldinteractive.ch>
2021-05-05 14:29:24 +0200

    Adjust endpoint strategy

--- packages/dynamic-content-pagination/src/strategies/JsonHtmlEndpoint.js
+++ packages/dynamic-content-pagination/src/strategies/JsonHtmlEndpoint.js
@@ -1,13 +1,15 @@
 import { json } from '@goldinteractive/js-base/src/utils/fetch'
+import { stringifyQuery } from '@goldinteractive/js-base/src/utils/url'
 
 class JsonHtmlEndpoint {
-  getData = (callback, node) => {
-    const url = node?.dataset?.actionUrl
-    if (!url) {
-      throw new Error(
-        `JsonHtmlEndpoint strategy needs an action url attribute on the node`
-      )
+  getData = (callback, node, state) => {
+    const actionUrl = node?.dataset?.actionUrl
+    if (!actionUrl) {
+      throw new Error(`JsonHtmlEndpoint strategy needs an action url attribute on the node`)
     }
+
+    const queryString = stringifyQuery(state)
+    const url = `${actionUrl}?${queryString}`
 
     json(url).then(result => {
       if (callback) {
