dfcc8824e4971c7f959b4f39023f7a989771c921
Michel Gabriel <michel.gabriel@goldinteractive.ch>
2021-05-17 10:56:37 +0200

    Finalize feature - add error handling - remove the not so default "default options" - add resetSkipState option

--- packages/dynamic-content-pagination/src/index.js
+++ packages/dynamic-content-pagination/src/index.js
@@ -1,26 +1,41 @@
 import { features } from '@goldinteractive/js-base'
 import { utils } from '@goldinteractive/js-base'
+import { noop } from '@goldinteractive/js-base/src/utils/fn'
 
 class DynamicContentPagination extends features.Feature {
   init() {
-    this.$content = this.$('[data-content]')
+    if (!this.options.strategy) {
+      throw new Error(
+        `DynamicContentPagination "${this.name}" feature needs to be initialized with a Strategy instance`
+      )
+    }
+
+    this.$content = this.$(this.options.contentSelector)
+    if (!this.$content) {
+      throw new Error(
+        `DynamicContentPagination "${this.name}" feature cannot find node element with ${this.options.contentSelector}`
+      )
+    }
+
     this.state = {}
 
     this.onHub(`${this.options.namespace}:state-update`, state => this.handleStateUpdate(state))
   }
 
   handleStateUpdate = state => {
-    // TODO: handle when filters changed reset skip
-    this.state = this.options.transformDomState(state)
+    const resetState = this.options.resetSkipState(this.node, this.state, state)
+    this.state = this.options.transformDomState(resetState)
     this.updateContent()
   }
 
   updateContent = () => {
+    this.options.onLoadContent({ node: this.node })
     this.options.strategy.getData(this.handleData, this.node, this.state)
   }
 
   handleData = data => {
     this.$content.innerHTML = data.html
+    this.options.onLoadedContent({ node: this.node })
     this.callPaginationStateHandler(data)
     this.callPaginationDisplayHandler(data)
   }
@@ -39,55 +54,19 @@ class DynamicContentPagination extends features.Feature {
 }
 
 DynamicContentPagination.defaultOptions = {
-  namespace: 'content-pagination',
+  namespace: 'dynamic-content-pagination',
   strategy: null,
+  contentSelector: '[data-content]',
   transformDomState: state => {
     return utils.url.parseQuery(state)
   },
-  paginationStateHandler: {
-    previous: (node, data, state) => {
-      const newState = { ...state } // Avoid reference
-      const $previous = node.querySelector('[data-previous]')
-      const prevSkip = data.meta.skip - data.meta.take
-      $previous.dataset.value = prevSkip
-      newState.skip = prevSkip
-      const queryString = utils.url.stringifyQuery(newState)
-      const url = location.origin + location.pathname + '?' + queryString + location.hash
-      $previous.href = url
-    },
-    next: (node, data, state) => {
-      const newState = { ...state } // Avoid reference
-      const $next = node.querySelector('[data-next]')
-      const nextSkip = data.meta.skip + data.meta.take
-      $next.dataset.value = nextSkip
-      newState.skip = nextSkip
-      const queryString = utils.url.stringifyQuery(newState)
-      const url = location.origin + location.pathname + '?' + queryString + location.hash
-      $next.href = url
-    }
+  resetSkipState: (node, oldState, newState) => {
+    return oldState
   },
-  paginationDisplayHandler: {
-    previous: (node, data) => {
-      const $previous = node.querySelector('[data-previous]')
-      if ($previous) {
-        if (parseInt(data.meta.skip) === 0) {
-          $previous.classList.add('-hide')
-        } else {
-          $previous.classList.remove('-hide')
-        }
-      }
-    },
-    next: (node, data) => {
-      const $next = node.querySelector('[data-next]')
-      if ($next) {
-        if (parseInt(data.meta.skip) + parseInt(data.meta.count) >= parseInt(data.meta.totalCount)) {
-          $next.classList.add('-hide')
-        } else {
-          $next.classList.remove('-hide')
-        }
-      }
-    }
-  }
+  onLoadContent: noop,
+  onLoadedContent: noop,
+  paginationStateHandler: {},
+  paginationDisplayHandler: {}
 }
 
 export default DynamicContentPagination
