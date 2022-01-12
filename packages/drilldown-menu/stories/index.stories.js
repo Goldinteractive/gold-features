49ac4f84104d9e74433290e0673a2237035b031a
Ramon Schumacher <ramon.schumacher@goldinteractive.ch>
2022-01-12 16:29:43 +0100

    DrilldownMenu: Update stories

--- packages/drilldown-menu/stories/index.stories.js
+++ packages/drilldown-menu/stories/index.stories.js
@@ -6,18 +6,19 @@ import {
   resetFeature
 } from '../../../helpers/story'
 
-import { features, eventHub } from '@goldinteractive/js-base'
+import { features } from '@goldinteractive/js-base'
 
 import DrilldownMenu from '../src/index'
 import '../src/style.scss'
 
 import docs from './docs.md'
 
-const markup = `
+const styling = `
 <style>
   .drilldown__menu {
     position: relative;
     overflow: hidden;
+    width: 200px;
   }
   .drilldown__submenu {
     position: absolute;
@@ -28,6 +29,7 @@ const markup = `
     background: green;
     transform: translateX(100%);
     transition: transform .3s;
+    width: 200px;
   }
   .drilldown__submenu--active {
     opacity: 1;
@@ -51,12 +53,16 @@ const markup = `
   }
   .drilldown__static-title {
     display: none;
+    font-weight: bold;
   }
   .drilldown__static-title--active {
     display: block;
   }
 </style>
+`
 
+const markupStatic = `
+${styling}
 <div data-feature="drilldown-menu">
   <button class="drilldown__static-back-btn" data-drilldown-back-static>Back (static)</button>
   <span class="drilldown__static-title" data-drilldown-title-static>Title (static)</span>
@@ -64,30 +70,60 @@ const markup = `
     <li class="drilldown__entry">
       <button data-drilldown-submenu-trigger data-drilldown-tab-target>Item 1</button>
       <ul class="drilldown__submenu" data-drilldown-submenu>
+        <li class="drilldown__entry"><a href="#1-1" data-drilldown-tab-target>Item 1-1</a></li>
+        <li class="drilldown__entry"><a href="#1-2" data-drilldown-tab-target>Item 1-2</a></li>
+        <li class="drilldown__entry">
+          <button data-drilldown-submenu-trigger data-drilldown-tab-target>Item 1-3</button>
+          <ul class="drilldown__submenu" data-drilldown-submenu>
+            <li class="drilldown__entry"><a href="#2-1" data-drilldown-tab-target>Item 2-1</a></li>
+            <li class="drilldown__entry"><a href="#2-2" data-drilldown-tab-target>Item 2-2</a></li>
+            <li class="drilldown__entry" data-drilldown-init-active><a href="#2-3" data-drilldown-tab-target>Item 2-3 (init active)</a></li>
+          </ul>
+        </li>
+        <li class="drilldown__entry"><a href="#1-4" data-drilldown-tab-target>Item 1-4</a></li>
+        <li class="drilldown__entry"><a href="#1-5" data-drilldown-tab-target>Item 1-5</a></li>
+        <li class="drilldown__entry"><a href="#1-6" data-drilldown-tab-target>Item 1-6</a></li>
+        <li class="drilldown__entry"><a href="#1-7" data-drilldown-tab-target>Item 1-7</a></li>
+        <li class="drilldown__entry"><a href="#1-8" data-drilldown-tab-target>Item 1-8</a></li>
+      </ul>
+    </li>
+    <li class="drilldown__entry"><a href="#2" data-drilldown-tab-target>Item 2</a></li>
+    <li class="drilldown__entry"><a href="#3" data-drilldown-tab-target>Item 3</a></li>
+    <li class="drilldown__entry"><a href="#4" data-drilldown-tab-target>Item 4</a></li>
+  </ul>
+</div>
+`
+const markupDynamic = `
+${styling}
+<div data-feature="drilldown-menu">
+  <ul class="drilldown__menu" data-drilldown-menu>
+    <li class="drilldown__entry">
+      <button data-drilldown-submenu-trigger data-drilldown-tab-target>Item 1</button>
+      <ul class="drilldown__submenu" data-drilldown-submenu>
         <button data-drilldown-back data-drilldown-tab-target>Back</button>
         <span>Title lvl1</span>
-        <li class="drilldown__entry"><a href="/1-1" data-drilldown-tab-target>Item 1-1</a></li>
-        <li class="drilldown__entry"><a href="/1-2" data-drilldown-tab-target>Item 1-2</a></li>
+        <li class="drilldown__entry"><a href="#1-1" data-drilldown-tab-target>Item 1-1</a></li>
+        <li class="drilldown__entry"><a href="#1-2" data-drilldown-tab-target>Item 1-2</a></li>
         <li class="drilldown__entry">
           <button data-drilldown-submenu-trigger data-drilldown-tab-target>Item 1-3</button>
           <ul class="drilldown__submenu" data-drilldown-submenu>
             <button data-drilldown-back data-drilldown-tab-target>Back</button>
             <span>Title lvl2</span>
-            <li class="drilldown__entry"><a href="/2-1" data-drilldown-tab-target>Item 2-1</a></li>
-            <li class="drilldown__entry"><a href="/2-2" data-drilldown-tab-target>Item 2-2</a></li>
-            <li class="drilldown__entry" data-drilldown-init-active><a href="/iframe.html" data-drilldown-tab-target>Item iframe!</a></li>
+            <li class="drilldown__entry"><a href="#2-1" data-drilldown-tab-target>Item 2-1</a></li>
+            <li class="drilldown__entry"><a href="#2-2" data-drilldown-tab-target>Item 2-2</a></li>
+            <li class="drilldown__entry" data-drilldown-init-active><a href="#2-3" data-drilldown-tab-target>Item 2-3 (init active)</a></li>
           </ul>
         </li>
-        <li class="drilldown__entry"><a href="/1-2" data-drilldown-tab-target>Item 1-4</a></li>
-        <li class="drilldown__entry"><a href="/1-2" data-drilldown-tab-target>Item 1-5</a></li>
-        <li class="drilldown__entry"><a href="/1-2" data-drilldown-tab-target>Item 1-6</a></li>
-        <li class="drilldown__entry"><a href="/1-2" data-drilldown-tab-target>Item 1-7</a></li>
-        <li class="drilldown__entry"><a href="/1-2" data-drilldown-tab-target>Item 1-8</a></li>
+        <li class="drilldown__entry"><a href="#1-4" data-drilldown-tab-target>Item 1-4</a></li>
+        <li class="drilldown__entry"><a href="#1-5" data-drilldown-tab-target>Item 1-5</a></li>
+        <li class="drilldown__entry"><a href="#1-6" data-drilldown-tab-target>Item 1-6</a></li>
+        <li class="drilldown__entry"><a href="#1-7" data-drilldown-tab-target>Item 1-7</a></li>
+        <li class="drilldown__entry"><a href="#1-8" data-drilldown-tab-target>Item 1-8</a></li>
       </ul>
     </li>
-    <li class="drilldown__entry"><a href="/2" data-drilldown-tab-target>Item 2</a></li>
-    <li class="drilldown__entry"><a href="/3" data-drilldown-tab-target>Item 3</a></li>
-    <li class="drilldown__entry"><a href="/4" data-drilldown-tab-target>Item 4</a></li>
+    <li class="drilldown__entry"><a href="#2" data-drilldown-tab-target>Item 2</a></li>
+    <li class="drilldown__entry"><a href="#3" data-drilldown-tab-target>Item 3</a></li>
+    <li class="drilldown__entry"><a href="#4" data-drilldown-tab-target>Item 4</a></li>
   </ul>
 </div>
 `
@@ -95,14 +131,45 @@ const markup = `
 storiesOf('DrilldownMenu', module)
   .addDecorator(withKnobs)
   .add(
-    'Intro',
+    'Static',
     () => {
-      return initializeDemo(markup, () => {
+      return initializeDemo(markupStatic, () => {
         resetFeature(features, 'drilldown-menu')
         features.add(
           'drilldown-menu',
           DrilldownMenu,
-          object('options', DrilldownMenu.defaultOptions)
+          object('options', {
+            namespace: 'drilldown-menu',
+            autoHeight: true,
+            staticBackBtn: true,
+            staticTitle: true,
+            openOnCurrentLevel: true
+          })
+        )
+        features.init(document.body)
+      })
+    },
+    {
+      notes: {
+        markdown: docs
+      }
+    }
+  )
+  .add(
+    'Dynamic',
+    () => {
+      return initializeDemo(markupDynamic, () => {
+        resetFeature(features, 'drilldown-menu')
+        features.add(
+          'drilldown-menu',
+          DrilldownMenu,
+          object('options', {
+            namespace: 'drilldown-menu',
+            autoHeight: true,
+            staticBackBtn: false,
+            staticTitle: false,
+            openOnCurrentLevel: true,
+          })
         )
         features.init(document.body)
       })
